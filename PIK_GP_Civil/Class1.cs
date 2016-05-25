525150e1049cb10a127745fae35a26b116b117a2
Vildar Khisyametdinov <vildar82@gmail.com>
2016-05-25 09:37:27 +0300

    Добавьте файлы проекта.

---
+++ PIK_GP_Civil/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIK_GP_Civil
+{
+    public class Class1
+    {
+    }
+}
